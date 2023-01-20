8504692a5bdc27d80cfa0587656a4b8d72a4fab4
maxgit <rotvel@gmail.com>
2023-01-20 19:25:38 +0100

    Added a bunch of record types taken from https://github.com/Aeroblast/UnpackKindleS

--- Source/MobiMetadata/EXTHHead.cs
+++ Source/MobiMetadata/EXTHHead.cs
@@ -71,6 +71,12 @@
         public uint RecordCount => GetPropAsUint(_recordCountAttr);
 
         //Records
+        public string DrmServerId => GetRecordAsString(1);
+
+        public string DrmCommerceId => GetRecordAsString(2);
+
+        public string DrmBookId => GetRecordAsString(3);
+
         public string Author => GetRecordAsString(100);
 
         public string Publisher => GetRecordAsString(101);
@@ -101,13 +107,36 @@
 
         public string VersionNumber => GetRecordAsString(114);
 
+        public uint Sample => GetRecordAsUint(115);
+
+        public uint StartOffset => GetRecordAsUint(116);
+
+        public string Adult => GetRecordAsString(117);
+
         public string RetailPrice => GetRecordAsString(118);
 
         public string RetailPriceCurrency => GetRecordAsString(119);
 
         public uint Kf8BoundaryOffset => GetRecordAsUint(121);
 
+        public string FixedLayout => GetRecordAsString(122);
+
         public string BookType => GetRecordAsString(123);
+
+        public string OrientationLock => GetRecordAsString(124);
+
+        /// <summary>
+        /// Fonts and images.
+        /// </summary>
+        public uint K8ResourcesCount => GetRecordAsUint(125);
+
+        public string OriginalResolution => GetRecordAsString(126);
+
+        public string ZeroGutter => GetRecordAsString(127);
+
+        public string ZeroMargin => GetRecordAsString(128);
+
+        public string K8MastheadCoverImage => GetRecordAsString(129);
 
         public uint RescOffset => GetRecordAsUint(131);
 
@@ -119,11 +148,68 @@
 
         public uint HasFakeCover => GetRecordAsUint(203);
 
+        public uint CreatorSoftware => GetRecordAsUint(204);
+
+        public uint CreatorMajorVersion => GetRecordAsUint(205);
+        
+        public uint CreatorMinorVersion => GetRecordAsUint(206);
+        
+        public uint CreatorBuildNumber => GetRecordAsUint(207);
+
+        public string WatermarkHexString => GetRecordAsString(208);
+
+        public string TamperProofKeysHexString => GetRecordAsString(209);
+
+        public string FontSignatureHexString => GetRecordAsString(300);
+
+        public uint ClippingLimit => GetRecordAsUint(401);
+
+        public uint PublisherLimit => GetRecordAsUint(402);
+
+        public uint TextToSpeechDisabled => GetRecordAsUint(404);
+
+        public uint RentalIndicator => GetRecordAsUint(406);
+
         public string CDEType => GetRecordAsString(501);
+
+        public string LastUpdateTime => GetRecordAsString(502);
 
         public string UpdatedTitle => GetRecordAsString(503);
 
         public string ASIN2 => GetRecordAsString(504);
+
+        public string TitleKatagana => GetRecordAsString(508);
+
+        public string CreatorKatagana => GetRecordAsString(517);
+
+        public string PublisherKatagana => GetRecordAsString(522);
+
+        public string Language => GetRecordAsString(524);
+
+        public string Unknown526 => GetRecordAsString(526);
+
+        public string PageProgressionDirection => GetRecordAsString(527);
+        
+        public string OverrideKindleFonts => GetRecordAsString(528);
+
+        public string Unknown529 => GetRecordAsString(529);
+
+        public string InputSourceType => GetRecordAsString(534);
+
+        public string CreatorBuildString => GetRecordAsString(535);
+
+        public string ContainerResolution => GetRecordAsString(538);
+
+        public string ContainerMimeType => GetRecordAsString(539);
+
+        public string UnknownButChangesWithFilename => GetRecordAsString(542);
+
+        /// <summary>
+        /// FONT_CONTAINER, BW_CONTAINER, HD_CONTAINER
+        /// </summary>
+        public string ContainerId => GetRecordAsString(543);
+
+        public string Unknown544 => GetRecordAsString(544);
 
         private string GetRecordAsString(uint recordType)
         {
